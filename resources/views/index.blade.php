a064dde3ed1ecabf217b1c218d46fae2b8e8cd39
amir-abbas ramezanipoor <amir.9980@yahoo.com>
2022-05-10 16:21:56 +0430

    fix index page cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,6 +13,7 @@
             <div class="card-body">
                 <h5 class="card-title">{{$product->title}}</h5>
                 <p class="card-text">{{\Illuminate\Support\Str::limit($product->description,20)}}</p>
+                <p>&nbsp;{{$product->price}}&nbsp;<del>{{$product->old_price}}</del> تومان</p>
             </div>
             <div class="d-flex card-footer justify-content-around mb-2">
                 <div class="col-sm-12 col-md-4">
@@ -21,6 +22,7 @@
                 <div class="col-sm-12 col-md-4">
                     <form action="{{route('cart.store',$product)}}" method="post">
                         @csrf
+
                         <button type="submit" name="addToCart" class="btn btn-warning btn-sm"><small>اضافه کردن به سبد</small></button>
 
                     </form>
